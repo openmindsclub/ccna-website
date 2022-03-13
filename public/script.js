548d7f63a76a62ee4eb5e8378cd91bd4c898566d
aymen-ghemam <gh.aymen39@gmail.com>
2022-03-14 00:56:45 +0100

    added favicon, setting the actual registration date

--- public/script.js
+++ public/script.js
@@ -4,23 +4,25 @@
       hour = minute * 60,
       day = hour * 24;
 
-    const countDown = new Date("03/19/2022").getTime(),
-      x = setInterval(() => {
-        const now = new Date().getTime();
-        distance = countDown - now;
+    const countDown = new Date(2022, 2, 16, 12, 0, 0).getTime();
+    const x = setInterval(() => {
+      const now = new Date().getTime();
+      distance = countDown - now;
 
-        document.getElementById("days").innerText = ("0" + Math.floor(distance / day)).slice(-2);
-        document.getElementById("hours").innerText = ("0" + Math.floor((distance % day) / hour)).slice(-2);
-        document.getElementById("minutes").innerText = ("0" + Math.floor((distance % hour) / minute)).slice(-2);
+      document.getElementById("days").innerText = ("0" + Math.floor(distance / day)).slice(-2);
+      document.getElementById("hours").innerText = ("0" + Math.floor((distance % day) / hour)).slice(-2);
+      document.getElementById("minutes").innerText = ("0" + Math.floor((distance % hour) / minute)).slice(-2);
 
-        //do something later when date is reached
-        if (distance < 0) {
-          document.getElementById("final").innerText = "Registrations are now closed.";
-          document.getElementById("register").style.display = "none";
-          document.getElementById("register").style.display = "none";
-          clearInterval(x);
-        }
-      }, 1000);
+      //do something later when date is reached
+      if (distance < 0) {
+        document.getElementById("final").innerText = "Registrations are now closed.";
+        document.getElementById("register").style.display = "none";
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0;
+        
+        clearInterval(x);
+      }
+    }, 1000);
   })();
 
 document.getElementById('form').onsubmit = (e) => {
